8016ebb33b875e841486f7987b8c9f2cd57807a2
Franco <ufrancis186@gmail.com>
2025-10-21 20:22:02 +0100

    omo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
 
   const [mobile, setMobile] = useState(false);
   return (
-    <div className="fixed z-50 bg-blue-950/90 top-4 w-[85vw] h-[50px] bg-transperent overflow-hidden flex flex-col justify-center justify-self-center rounded-md mx-auto">
+    <div className="fixed z-50 top-4 left-1/2 -translate-x-1/2 w-[85vw] h-[50px] bg-blue-950/90 overflow-hidden flex flex-col justify-center rounded-md mx-auto">
 
       <div className="flex flex-row md:justify-around justify-between items-center h-full">
         <div className="pl-10">
